a853214808a4a65589fe441bff21ed84911da0ae
shixiang <785263824@qq.com>
2016-09-25 16:59:50 +0800

    9.25

---
+++ js/js/box.js
@@ -0,0 +1,19 @@
+var guanli = {
+	
+	ele : null,
+	
+	init : function(){
+		
+		this.bgmove : function(){
+			
+			
+		}
+	},
+	
+	bgmove : function(){
+		var y=0 ;
+		var box = document.getElementById("box");
+		box.style.baca
+		
+	}
+}
